5cf64407d6f35c2cf389e8ddd7fc4c922cb4d3a8
Leandro Gabriel Faria <leandrogabrielfaria05@gmail.com>
2023-08-23 21:49:38 -0300

    método criar matriz

--- Flood_Fill/src/matriz/Matrix.java
+++ Flood_Fill/src/matriz/Matrix.java
@@ -1,4 +1,38 @@
 package matriz;
 
 public class Matrix {
+    private int coluna;
+    private int linha;
+    private int matriz[][];
+
+    void matriz(int coluna, int linha) {
+        setColuna(coluna);
+        setLinha(linha);
+        montarMatriz();
+    }
+
+    private void setColuna(int x) {
+        coluna = 10;
+    }
+
+    private void setLinha(int x) {
+        linha = 10;
+    }
+
+    private int getColuna(){
+        return coluna;
+    };
+    private int getLinha(){
+        return linha;
+    };
+
+    public void montarMatriz() {
+        matriz = new int [coluna][linha];
+        for (int i=0; i<matriz[i].length - 1;i++) {
+            for(int y=0; y<matriz[y].length - 1;y++) {
+                matriz[i][y] = 0;
+            }
+
+        }
+    }
 }

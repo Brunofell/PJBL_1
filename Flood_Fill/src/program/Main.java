06b35e79417e28e706d0081d2c86bd798b881e69
Bruno Feliciano <104511327+Brunofell@users.noreply.github.com>
2023-08-25 16:57:06 -0300

    Fiz a matrix, e o metodo do floodfill

--- Flood_Fill/src/program/Main.java
+++ Flood_Fill/src/program/Main.java
@@ -1,10 +1,26 @@
 package program;
 import matriz.Matriz;
+import java.util.Scanner;
+
 
 
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        Matriz matriz = new Matriz(10,10);
+        matriz.montaMatriz();
+        System.out.println("Digite as coordenada: ");
+        System.out.print("X(linha): ");
+        int x = scanner.nextInt();
+        System.out.print("Y(coluna): ");
+        int y = scanner.nextInt();
+
+       // matriz.floodFill(x, y);
 
     }
 

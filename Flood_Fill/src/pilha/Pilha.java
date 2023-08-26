551bde36575be1c01a8fe96c23a3353ed2de813a
Leandro Gabriel Faria <leandrogabrielfaria05@gmail.com>
2023-08-26 16:55:11 -0300

    consertando a pilha

--- Flood_Fill/src/pilha/Pilha.java
+++ Flood_Fill/src/pilha/Pilha.java
@@ -12,62 +12,63 @@ public class Pilha<T> {
     public Pilha(int size) {
         this.stack = (T[]) new Object[size];
         this.size = size;
-        this.top = -1;
+        this.top = -1;          //Topo = -1 significa que a pilha está vazia
         this.isFull = false;
         this.isEmpty = true;
     }
 
-    public void add(T value) {
-        if (top < this.stack.length && !isFull()) {
-            stack[++top] = value;
-            isEmpty = false; // set to false when adding an element
-        } else{; // set to true when the stack is full
+    public void adicionar(T value) {
+
+        //Se o topo for menor que a pilha e ela não estiver cheia, adiciona um valor no topo e aumenta o indíce dele
+
+        if (top < this.stack.length && !estaCheia())  { 
+            stack[++top] = value;       
+            isEmpty = false; 
+        } else{; 
             System.out.println("A pilha está cheia. Não é possível adicionar.");
         }
     }
 
     public T remover() {
-        if (!isEmpty()) {  // Verifique se a pilha não está vazia antes de tentar remover
+
+        //Se a pilha não estiver vazia, ele vai remover o valor localizado no topo, vai diminuir o indíce do top e vai definir o isfull como falso, além de retornar o valor removido.
+
+        if (!estaVazia()) {  
             T a = stack[top];
-            stack[top] = null;  // Defina o elemento no topo como 0 (opcional, mas comum)
-            top--;  // Reduza o índice do topo para apontar para o próximo elemento
+            stack[top] = null;  
+            top--;  
             isFull=false;
             return a;
 
         } else {
             System.out.println("A pilha está vazia. Não é possível remover.");
-            return null;  // Ou você pode lançar uma exceção, dependendo do seu design.
-        }
-    }
-    public void printStack() {
-        System.out.println("Stack:");
-        for (int i = 0; i <= top; i++) {
-            System.out.println(stack[i]);
+            return null;  
         }
     }
 
-
-
-
-
-
-    public boolean isEmpty() {
-        return isEmpty; // simplified
+    //Método para verificar se a pilha está vazia
+    public boolean estaVazia() {
+        return isEmpty; 
     }
 
-    public boolean isFull() {
+    //Método para verificar se a pilha está cheia
+    public boolean estaCheia() {
         if (top == size - 1) {
             return true;
         }
         return false;
     }
 
-    public void clear() {
-        top = -1;
-        isEmpty = true; // set to true when clearing the stack
-        isFull = false; // set to false when clearing the stack
-    }
+    //Método para limpar a pilha
+    public void limpar() {
+        for (int i = 0; i <= top; i++) {
+            stack[i] = null;  
+        }
 
+        top = -1;  
+        isEmpty = true;  
+        isFull = false;  
+    }
 
 }
 
